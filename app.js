5d72df4d222b817e1cd5a9cdadb3b00d20f349e4
c0co4ik <ponomarew.matv@yandex.ru>
2022-12-17 15:02:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+async function getDashboardData(url = '/data.json'){
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+class DashboardItem {
+  static PERIODS = {
+    daily: "day",
+    weekly: "week",
+    monthly: "mounth",
+  };
+
+  constructor(data, container = ".dashboard__content", view = "weekly") {
+    this.data = data;
+    this.container = document.querySelector(container);
+    this.view = view;
+    this.createMarkup();
+  }
+
+  createMarkup() {
+    const { title, timeframes } = this.data;
+    const id = title.toLowerCase().replace(/ /g, "-");
+    const { current, previous } = timeframes[this.view.toLowerCase()];
+
+    this.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="dashboard__item dashboard__item--${id}">
+        <article class="tracking-card">
+          <header class="tracking-card__header">
+            <h4 class="tracking-card__title">${title}</h4>
+            <img src="images/icon-ellipsis.svg" alt="menu" class="tracking-card__img">
+          </header>
+          <div class="tracking-card__body">
+            <div class="tracking-card__time">
+              ${current}hrs
+            </div>
+            <div class="tracking-card__period">
+              Last ${DashboardItem.PERIODS[this.view]} - ${previous}hrs
+            </div>
+          </div>
+        </article>
+      </div>`
+    );
+
+    this.time = this.container.querySelector(
+      `.dashboard__item--${id} .tracking-card__time`
+    );
+    this.prev = this.container.querySelector(
+      `.dashboard__item--${id} .tracking-card__period`
+    );
+  }
+
+  changeView(view) {
+    this.view = view.toLowerCase();
+    const { current, previous } = this.data.timeframes[this.view.toLowerCase()];
+
+    this.time.innerText = `${current}hrs`;
+    this.prev.innerText = `Last ${
+      DashboardItem.PERIODS[this.view]
+    } - ${previous}hrs`;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  getDashboardData()
+    .then(data => {
+      const activities = data.map(activity => new DashboardItem(activity));
+
+      const selectors = document.querySelectorAll(".value-person__btn");
+      
+      selectors.forEach(selector => 
+        selector.addEventListener('click', function () {
+          selectors.forEach((sel) => sel.classList.remove("value-person__btn--active"));
+          selector.classList.add("value-person__btn--active");
+
+          const currentView = selector.innerText.trim().toLowerCase();
+          activities.forEach(activity => activity.changeView(currentView));
+        }))
+    })
+})
